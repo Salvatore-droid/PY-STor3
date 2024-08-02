9a52e67e357d2ff0ea78b09fe9e59cbab608a980
Salvatore-droid <geniusokwemba53@gmail.com>
2024-08-03 00:40:42 +0300

    Linux automated app downloads by PY-STor3

---
+++ py-store.py
@@ -0,0 +1,118 @@
+import pyfiglet
+import subprocess
+import sys
+import time
+from rich.console import Console
+
+console = Console()
+
+def banner():
+    text = "[[<<\...PY-STore../>>]"
+    banner=pyfiglet.figlet_format(text)
+    console.print(banner, style="cyan")
+banner()
+console.print('''
+            ==[[....PY-STore v-1.0....]]==
+
+        [[\>>>>.. Easier Linux apps access.. <<<</]]
+
+      [++[[...Created by Genius ( Salvatore-droid )...]]++]
+''', style="yellow")
+
+
+def flatpak_search(app):
+    try:
+        subprocess.run(["flatpak", "search", app], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error, search not found {app}: {e}")
+        sys.exit(1)
+
+def run_app(app_id):
+    try:
+        subprocess.run(["flatpak", "run", app_id], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occured: {e}")
+        sys.exit(1)
+
+def check_flatpak():
+    try:
+        subprocess.run(["which", "flatpak"], 
+        stdout=subprocess.DEVNULL, 
+        stderr=subprocess.DEVNULL, 
+        text=True,
+        check=True
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def run_command(command):
+    try:
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True, check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        return False, e.stderr
+
+def install_flatpak():
+    print("Updating system")
+    run_command(["sudo", "apt", "update"])
+    line()
+    print("\nInstalling flatpak...")
+    run_command(["sudo", "apt", "install", "-y", "flatpak"])
+    run_command(["flatpak", "remote-add", "--if-not-exists", "flathub", "https://flathub.org/repo/flathub.flatpakrepo"])
+    line()            
+
+
+def install_app(app_id):
+    try:
+        subprocess.run(["flatpak", "install", "flathub", app_id], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"app not found:{e}")
+        sys.exit(1)
+
+def delay():
+    for _ in range(100):
+        sys.stdout.write("*")
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+def line():
+    for _ in range(60):
+        sys.stdout.write("-")
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+
+def main():
+    delay()
+    print('''
+                        
+                     <++\ ..app installation usage.. /++>                                                         |              <++ ..app running usage.. ++>
+                                                                                                                  |
+        [< .. Enter app name: (example) wps .. >]                                                                 | [==[ ..use the comand : flatpak run (Application ID).. ]==]
+        [< .. Search results for 'wps'.. >]-------]                                                               |
+        [< .. Name              Application ID             Version            Branch       Installation .. >]     | **NOTE:-After installation, the app is ran automatically by py-store
+             WPS Office        com.wps.Office             11.1.0.11719       stable       system                  |        -Use above command to run app in terminal  
+        [[.. use the Application ID (com.wps.Office) for installation of the app ..]]                             | 
+                                                                                                                  |
+                                                                                                                  |
+    ''')
+    console.print("        **NOTE: Application ID is Key sensitive, Enter the exact Application id to install                        | **NOTE:To view all installed apps, run: flatpak list     \n", style="red")
+    delay()
+    app = input("\n       Enter the app name to search: ")
+    flatpak_installed = check_flatpak()
+    if flatpak_installed:
+        console.print("Installed flatpak", style="yellow")
+    else:
+        install_flatpak()
+    flatpak_search(app)
+    app_id = input("\nEnter Application ID to install the app: ")
+    install_app(app_id)
+    run_app(app_id)
+
+if __name__ == "__main__":
+    main()
+
+    
+
